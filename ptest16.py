a01e619a16c8a04c8cef1145c2b7062db01c1be6
Willie Mak <willie.mak@outlook.com>
2023-06-20 16:48:19 +0800

    620

--- ptest16.py
+++ ptest16.py
@@ -1,2 +1,6 @@
+import math
+import datetime
 x = 123
+y=datetime.datetime.now() 
+print (y)
 print (f'this is the testing of replacement variable {x}')
--- ptest3.py
+++ ptest3.py
@@ -1,5 +1,5 @@
 import sys
 print (sys.argv)
 print (sys.argv[0])
-print (sys.argv[1])
+print('hello')
 
